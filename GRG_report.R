68f2288de9ec382acd9c4055f65fc2945edacfc9
Elaine C Evans <elainee@umn.edu>
2025-03-26 12:58:32 -0500

    update report for 2024 data

--- GRG_report.R
+++ GRG_report.R
@@ -1,18 +1,37 @@
 ###report for Great River Greening bumble bee surveys
 library(dplyr)
+library(vegan)
+library(fossil)
+library(gt)
 
 
 ####all bumble bee data 2022 to 2024
-bees=read.csv("G:/My Drive/R/GreatRiverGreening/greatrivergreeningbumblebees/cleaned_GRGbumblebeedata.csv")
+bees=read.csv("G:/My Drive/R/greatrivergreeningbumblebees/cleaned_GRGbumblebeedata.csv")
 
 ###Ritter data 2022 to 2024
 ritterbees <- bees[ which(bees$site=='Ritter_Farm'), ]
 
 ####surveyeffort
-effort=read.csv("G:/My Drive/R/GreatRiverGreening/greatrivergreeningbumblebees/collectiondata.csv")
+effort=read.csv("G:/My Drive/R/greatrivergreeningbumblebees/collectiondata.csv")
 
-###table of bees by year
+###species matrix
+rittermatrix <- create.matrix(ritterbees, tax.name = "BumbleBeeSpecies",
+                    locality = "collectioncode",
+                    abund.col = "Abundance",
+                    abund = TRUE)
 
-  group_by(ritterbees,year) %>%
-  summarise(total = sum(Abundance), richness=count(BumbleBeeSpecies))
+##species accululation
+sp1=specaccum(rittermatrix, method = "exact", permutations = 100,
+          conditioned =TRUE, gamma = "jack1",  w = NULL)
+
+plot(sp1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue",xlab="collection", ylab = "richness")
+
+#table by year
+
+ritteryear <- create.matrix(ritterbees, tax.name = "BumbleBeeSpecies",
+                              locality = "year",
+                              abund.col = "Abundance",
+                              abund = TRUE, )
+
+
 
