54d6f141bc1425405f7d8e7d26b8ad2204ee9b5c
Elaine C Evans <elainee@umn.edu>
2025-03-31 13:17:13 -0500

    Added notes for report

--- GRG_report.R
+++ GRG_report.R
@@ -2,17 +2,20 @@
 library(dplyr)
 library(vegan)
 library(fossil)
-library(gt)
+library(pivottabler)
+library(janitor)
+library(viridis)
+library(bipartite)
 
 
 ####all bumble bee data 2022 to 2024
-bees=read.csv("G:/My Drive/R/greatrivergreeningbumblebees/cleaned_GRGbumblebeedata.csv")
+bees=read.csv("cleaned_GRGbumblebeedata.csv")
 
 ###Ritter data 2022 to 2024
 ritterbees <- bees[ which(bees$site=='Ritter_Farm'), ]
 
 ####surveyeffort
-effort=read.csv("G:/My Drive/R/greatrivergreeningbumblebees/collectiondata.csv")
+effort=read.csv("samplingeffort.csv")
 
 ###species matrix
 rittermatrix <- create.matrix(ritterbees, tax.name = "BumbleBeeSpecies",
@@ -32,6 +35,48 @@ ritteryear <- create.matrix(ritterbees, tax.name = "BumbleBeeSpecies",
                               locality = "year",
                               abund.col = "Abundance",
                               abund = TRUE, )
+
+pt <- PivotTable$new()
+pt$addData(ritterbees)
+pt$addColumnDataGroups("year")
+pt$addRowDataGroups("BumbleBeeSpecies")
+pt$defineCalculation(calculationName="Abundance", summariseExpression="sum(Abundance)")
+pt$renderPivot()
+
+
+#### relative abundance
+
+relabund=ritterbees %>%
+  tabyl(year, BumbleBeeSpecies) %>%
+  adorn_totals("col") %>% 
+  adorn_percentages() 
+
+ relabund=adorn_rounding(relabund, digits = 2, rounding = "half to even")
+
+###transpose
+transprelabund=relabund[-1] %>% t() %>% as.data.frame() %>% setNames(relabund[,1])
+
+#####format table outside of R with relative abudance
+
+
+
+##bees per volunteer hour...box and whisker plots
+
+
+
+
+###plants visited by bees....graphs with relative abundance of bees on flowers. 
+###heat color with bb species richness
+
+                                              
+
+
+
+###proportion of visits to native and non-native plants. compare ritter across years.
+
+
+
+##interaction plots....use bipartite package
 
 
 
