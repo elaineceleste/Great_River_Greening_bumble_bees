40cdf2eec9d77e3c2c30de11e700677548e528f8
Elaine Evans <fuzzybumblebee@gmail.com>
2026-03-04 22:13:41 -0600

    Updated formatting for charts. Redid network web plot to make it work as a horizontal plot. Analyzed bees per volunteer per minute.

--- GRG_report.R
+++ GRG_report.R
@@ -7,6 +7,7 @@ library(janitor)
 library(viridis)
 library(bipartite)
 library(tidyr)
+library(emmeans)
 
 
 ####all bumble bee data 2022 to 2025
@@ -24,6 +25,7 @@ ritterbees2 <- ritterbees %>%
 
 ####surveyeffort
 effort=read.csv("cleanedGRGSamplingEffort22-25.csv")
+effort <- effort[ which(effort$LocationCode=='Ritter'), ]
 
 ###species matrix
 rittermatrix <- create.matrix(ritterbees, tax.name = "BumbleBeeSpecies",
@@ -32,11 +34,13 @@ rittermatrix <- create.matrix(ritterbees, tax.name = "BumbleBeeSpecies",
                     abund = TRUE)
 
 
-##species accululation
+##species accumulation
 sp1=specaccum(rittermatrix, method = "exact", permutations = 100,
           conditioned =TRUE, gamma = "jack1",  w = NULL)
 
-plot(sp1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue",xlab="collection", ylab = "richness")
+plot(sp1, ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue",
+     xlab="Collection number", ylab = "Bumble bee richness +/- 95% C.I.", 
+     main = "Species accumulation")
 
 #table by year
 
@@ -77,10 +81,11 @@ ggplot(ritterbees, aes(x = factor(year), fill = BumbleBeeSpecies)) +
   geom_bar(position = "fill") +
   scale_y_continuous(labels = scales::percent) +
   scale_fill_brewer(palette = "PuOr") + #Using a colorblind friendly palette
-  labs(title = "Bumble Bee Species Abundance by Year", 
+  labs(title = "Bumble bee species abundance by year", 
        x = "Year",
-       y = "Relative Abundance",
-       fill = "Bumble Bee Species") +
+       y = "Relative abundance",
+       fill = "Bumble bee species") +
+  theme(legend.text = element_text(face = "italic"))
   theme_minimal()
 
 
@@ -90,11 +95,14 @@ fm1
 summary(fm1)
 plot(fm1)
 
+#abundance square root transformed to meet assumptions of normality
 fm2=aov(sqrt(totalbeesobserved) ~ year, data = effort)
 summary(fm2)
 plot(fm2)
+boxplot(effort$totalbeesobserved ~ effort$year,xlab="Year", ylab = "Average abundance per collection +/- S.E." )
 
-boxplot(effort$totalbeesobserved ~ effort$year,xlab="Year", ylab = "Average abundance per collection" )
+#still not normal. If we want to do this comparison need to use a non-parametric test
+
 
 # Add data points
 #mylevels <- levels(as.factor(effort$totalbeesobserved))
@@ -107,10 +115,14 @@ boxplot(effort$totalbeesobserved ~ effort$year,xlab="Year", ylab = "Average abun
 
 ##getting data into matrix of plant species, abundance. does not include bumble bees
 
+
+
+###Network
 beenetwork=ritterbees2
 networktable=   beenetwork%>%
   group_by(FlowerName, BumbleBeeSpecies) %>%
   summarize(abundance = sum(Abundance))  
+
 
 ##going from long to wide
 plotnetwork=networktable %>% 
@@ -118,6 +130,8 @@ plotnetwork=networktable %>%
               values_from = abundance)
 
 plotnetwork_mat <- as.matrix(plotnetwork[,-1])
+#replace NA with 0
+plotnetwork_mat[is.na(plotnetwork_mat)] <- 0
 
 #add plant names back into matrix
 
@@ -133,17 +147,16 @@ plotnetwork_mat = plotnetwork_mat[-1,]
 ##Transpose matrix so bees are rows and plants are columns
 plotnetwork_mat_flipped <- t(plotnetwork_mat)
 
-plotweb(plotnetwork_mat_flipped,
-       method = "normal",
-       text.rot = 90,
-       col.low = "skyblue",     # left boxes (now bees)
-       col.high = "orange",     # right boxes (now plants)
-       col.interaction = "grey40",
-       bor.col.interaction = "black") 
+plotweb(plotnetwork_mat_flipped, horizontal = TRUE, 
+        spacing = "auto", 
+        higher_italic = TRUE, lower_italic = TRUE,
+        text_size = 0.75, curved_links = TRUE)
 
 
 
-##bees per volunteer minute...box and whisker plots
+
+
+##bees per volunteer minute...box and whisker plots with color
 
 effort <- effort %>%
   mutate(
@@ -151,6 +164,8 @@ effort <- effort %>%
   ) #this column was already added in excel (see beespervolunteerperminute) but also wanted to make sure I could do it in r. 
 
 library(ggplot2)
+
+
 
 ggplot(effort, aes(x = factor(year), 
                    y = bees_per_vol_per_min, 
@@ -164,25 +179,46 @@ ggplot(effort, aes(x = factor(year),
   guides(fill = "none") +
   labs(
     x = "Year",
-    y = "Bees per Volunteer per Minute"
-  ) 
+    y = "Bees per Volunteer per Hour", title="Bee capture rate",
+  ) +
+  theme(plot.title = element_text(hjust = 0.5))
  
-##bees per volunteer per hour....box and whisker plots
+##bees per volunteer per hour....box and whisker plots without color
 
 ggplot(effort, aes(x = factor(year), 
                    y = beespervolunteerperhour, 
-                   fill = factor(year))) +
-  geom_boxplot() +
-  scale_fill_manual(values = c("2022" = "#1b9e77",
-                               "2023" = "#d95f02",
-                               "2024" = "#7570b3",
-                               "2025" = "#e7298a")) +
+                   )) +
+  geom_boxplot()  +
   theme_classic() +
   guides(fill = "none") +
   labs(
     x = "Year",
-    y = "Bees per Volunteer per Hour") +
+    y = "Bees per volunteer per hour", title="Bee capture rate",
+  ) +
+  theme(plot.title = element_text(hjust = 0.5)) +
   coord_cartesian(ylim = c(0,10))
+
+##bee abundance over time
+fm3=aov(beespervolunteerperhour ~ year, data = effort)
+fm3
+summary(fm3)
+plot(fm3)
+
+#transform to normalize
+fm3=aov(log(beespervolunteerperhour) ~ year, data = effort)
+fm3
+summary(fm3)
+plot(fm3)
+
+##not normal...trying kruskal wallis
+kruskal.test(beespervolunteerperhour ~ year, data = effort)
+
+#comparing just 2022 and 2025
+target <- c("2022", "2025")
+effort2225=filter(effort, year %in% target)
+fm4=kruskal.test(beespervolunteerperhour ~ year, data = effort2225)
+fm4
+summary(fm4)
 
 
 ##summarize bee abundance by plant
